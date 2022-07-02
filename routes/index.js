64577f15eb1e60e44c01ee0ba8a1405019680351
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-07-02 17:05:04 +0200

    end-iteration-3

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,19 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => {
+
+    // retrouver tous les films a passer au template
+    Movie.find()
+      .then(function (filmsFromDB) {
+
+        res.render('movies', {
+            movies: filmsFromDB
+        })
+      })
+      .catch()
+
+    
+});
+
 module.exports = router;
