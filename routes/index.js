9ab851324d553262f07c5a898f5ac35ecfdfdf63
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-07-04 20:54:36 +0200

    in-progress

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/movie.model");
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
@@ -9,14 +10,13 @@ router.get('/movies', (req, res, next) => {
     // retrouver tous les films a passer au template
     Movie.find()
       .then(function (filmsFromDB) {
-
         res.render('movies', {
-            movies: filmsFromDB
+            films: filmsFromDB
         })
       })
-      .catch()
-
-    
+      .catch(function () {
+        console.log('error to load films')
+      })
 });
 
 module.exports = router;
